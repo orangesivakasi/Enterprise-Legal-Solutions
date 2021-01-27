a1e3568b19a28ea4052ff11fc5d49368124404b6
orangesivakasi <76803126+orangesivakasi@users.noreply.github.com>
2021-01-27 12:38:32 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $("#myModal").modal('show');
+});
+
+$(document).ready(function(){
+    $(this).scrollTop(0);
+});
+
+$(document).ready(function(){
+	$(window).scroll(function () {
+			if ($(this).scrollTop() > 50) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		// scroll body to 0px on click
+		$('#back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 400);
+			return false;
+		});
+});
+
